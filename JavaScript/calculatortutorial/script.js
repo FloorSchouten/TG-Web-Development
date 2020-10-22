3be0b03e4c801cf435e21bb5cf4f72b75c9ce0ad
Floor Schouten <corporatecontent@outlook.com>
2020-10-22 10:25:57 +0200

    JS calculatortutorial

---
+++ JavaScript/calculatortutorial/script.js
@@ -0,0 +1,46 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement){
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
+    }
+}
+
+clear() {
+    this.previousOperand =''
+    this.currentOperand = ''
+    this.operation = undefined
+
+}
+
+delete() {
+
+}
+
+appendNumber() {
+
+}
+chooseOperation(operation){
+
+}
+compute() {
+
+}
+updateDisplay() {
+    this.currentOperandTextElement.innerText = this.currentOperand
+
+}
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelectorAll('[data-previous-operand]')
+const currentOperandTextElement = document.querySelectorAll('[data-current-operand]')
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculatro.updateDisplay
+})
+})
