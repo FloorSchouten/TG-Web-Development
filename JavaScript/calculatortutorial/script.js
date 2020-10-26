e08a33948a512952bae4baee1873aa2c58509072
Floor Schouten <corporatecontent@outlook.com>
2020-10-26 13:15:20 +0100

    calculator

--- JavaScript/calculatortutorial/script.js
+++ JavaScript/calculatortutorial/script.js
@@ -32,7 +32,7 @@ updateDisplay() {
 }
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
-const equalsButton = document.querySelector('[data-equals]')
+const equalsButton = document.querySelector('[data-equals]')//single
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelectorAll('[data-previous-operand]')
 const currentOperandTextElement = document.querySelectorAll('[data-current-operand]')
