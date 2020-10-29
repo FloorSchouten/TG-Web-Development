a390628ed382412f8c790761bb0ee291a270c548
Floor Schouten <corporatecontent@outlook.com>
2020-10-29 18:01:25 +0100

    React course

---
+++ JavaScript/REACT/hello-world/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Greet from './components/Greet'
+import Welcome from './components/Welcome'
+import Stylesheet from './components/Stylesheet'
+
+  class App extends Component {
+    render () {
+      return (
+        <div className="App">
+          
+          <Stylesheet primary={false} />
+          <Greet/>
+          <Welcome />
+        </div>
+      );
+    }
+}
+
+export default App;
---
+++ JavaScript/REACT/hello-world/src/components/Greet.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+// function Greet() {
+//     return <h1>Hello Herootje</h1>
+// }
+
+const Greet = () => <h1>Hello Herootje</h1>
+export default Greet
