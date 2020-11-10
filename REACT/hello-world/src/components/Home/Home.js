f43a43f3be590bcf53c75ea4f354ffa3291c3291
Floor Schouten <corporatecontent@outlook.com>
2020-11-10 13:44:22 +0100

    Update Home.js

--- REACT/hello-world/src/components/Home/Home.js
+++ REACT/hello-world/src/components/Home/Home.js
@@ -9,7 +9,7 @@ import Jumbotron from 'react-bootstrap/Jumbotron'
         <Jumbotron className="color-home" id="Home">
                     <div></div>
                         <h1>Nice to meet you!</h1>
-                        <p className="paragraphBox">Look at this website! It's made in React with Bootstrap components. The navigation bar, the jumbotron segments and the carousel all work nicely. In the contact segment, you can add contact information and links to social networks. 
+                        <p className="paragraphBox">Look at this website: it's made in React with Bootstrap components. The navigation bar, the jumbotron segments and the carousel all work nicely. In the contact segment, you can add contact information and links to social networks. 
                         </p>
                     
                 </Jumbotron>);
